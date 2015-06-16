68cb2012bd4e46bc21dd7a3c225509dc5bfa5742
laoratorio <laoratorio@atl-labobd-304>
2015-06-15 19:55:35 -0430

    primer commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,19 @@
+package javaapplication1;
+
+public class JavaApplication1 {
+    
+    private int contador = 0;
+
+    public static void main(String[] args) {
+
+        JavaApplication1 jpl = new JavaApplication1();
+        
+        
+        
+        for (jpl.contador = 0; jpl.contador< 10; jpl.contador++) {
+            System.out.println("imprimiendo el contador\n\n"+jpl.contador);
+        }
+
+    }
+
+}
